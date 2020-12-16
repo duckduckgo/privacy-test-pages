0516cfd04d49ecfd3ed07abdb0accc33fedac789
Konrad Dzwinel <kdzwinel@gmail.com>
2020-12-16 16:42:45 +0100

    HTTPS upgrades - first batch

---
+++ privacy-protections/https-upgrades/main.js
@@ -0,0 +1,65 @@
+const startButton = document.querySelector('#start');
+const downloadButton = document.querySelector('#download');
+
+const testsDiv = document.querySelector('#tests');
+const testsSummaryDiv = document.querySelector('#tests-summary');
+const testsDetailsDiv = document.querySelector('#tests-details');
+
+// object that contains results of all tests
+const results = {
+    page: 'https-upgrades',
+    date: null,
+    results: []
+};
+
+/**
+ * Test runner
+ */
+function runTests() {
+    startButton.setAttribute('disabled', 'disabled');
+    downloadButton.removeAttribute('disabled');
+    testsDiv.removeAttribute('hidden');
+
+    results.results.length = 0;
+    results.date = (new Date()).toUTCString();
+    let all = 0;
+    let failed = 0;
+
+    testsDetailsDiv.innerHTML = '';
+
+    function updateSummary() {
+        testsSummaryDiv.innerText = `Performed ${all} tests${failed > 0 ? ` (${failed} failed)` : ''}. Click for details.`;
+    }
+
+    tests.forEach(test => {
+    });
+
+
+    updateSummary();
+
+    startButton.removeAttribute('disabled');
+}
+
+function downloadTheResults() {
+    const data = JSON.stringify(results, null, 2);
+    const a = document.createElement('a');
+    const url = window.URL.createObjectURL(new Blob([data], {type: 'application/json'}));
+    a.href = url;
+    a.download = 'fingerprinting-results.json';
+    
+    document.body.appendChild(a);
+    a.click();
+
+    window.URL.revokeObjectURL(url);
+    a.remove();
+}
+
+downloadButton.addEventListener('click', () => downloadTheResults());
+
+// run tests if button was clicked or…
+startButton.addEventListener('click', () => runTests());
+
+// if url query is '?run'
+if (document.location.search === '?run') {
+    runTests();
+}

148098fa90c0538411ec9bae383fba681b51d8dd
Sam Macbeth <smacbeth@duckduckgo.com>
2021-02-09 15:15:35 +0100

    Add storage test for cookies set from a 3rd party script.

---
+++ privacy-protections/storage-blocking/3rdparty.js
@@ -0,0 +1,15 @@
+/**
+ * document.cookie test run in a 3rd party script.
+ * 
+ * As we load this script via a 3rd party origin in the test, when running `store` the 3rd party
+ * origin will be visible in the call stack to document.cookie.
+ */
+tests.push({
+    id: 'JS cookie (3rd party script)',
+    store: (data) => {
+        document.cookie = `tpdata=${data}; expires= Wed, 21 Aug 2030 20:00:00 UTC;`;
+    },
+    retrive: () => {
+        return document.cookie.match(/tpdata\=([0-9]+)/)[1];
+    }
+});

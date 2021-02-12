fca6d13529a0dbe8b7aefa3fc33f6f0e0f7a0808
Sam Macbeth <smacbeth@duckduckgo.com>
2021-02-12 11:08:15 +0100

    Support store on load for 3rd party js cookie

--- privacy-protections/storage-blocking/3rdparty.js
+++ privacy-protections/storage-blocking/3rdparty.js
@@ -4,7 +4,7 @@
  * As we load this script via a 3rd party origin in the test, when running `store` the 3rd party
  * origin will be visible in the call stack to document.cookie.
  */
-tests.push({
+commonTests.push({
     id: 'JS cookie (3rd party script)',
     store: (data) => {
         document.cookie = `tpdata=${data}; expires= Wed, 21 Aug 2030 20:00:00 UTC;`;

0d325fe216d0949a26d3c3d51b9de6d53bcec574
jdorweiler <jason@duckduckgo.com>
2021-03-04 12:57:22 -0700

    update test

--- privacy-protections/surrogates/main.js
+++ privacy-protections/surrogates/main.js
@@ -1,40 +1,37 @@
 function loadSurrogates () {
 
-    for (const [name, test] of Object.entries(surrogates)) {
+    for (const [name, testData] of Object.entries(surrogates)) {
         const s = document.createElement('script')
-        test.crossOrigin ? s.crossOrigin = test.crossOrigin : ''
-        s.src = test.url
+        testData.crossOrigin ? s.crossOrigin = testData.crossOrigin : ''
+        s.src = testData.url
         
-        s.onload = () => updateTable({name, test: test.test, notes: test.notes})
-        s.onerror = (error) => updateTable({name, error, test: test.test, notes: test.notes, shouldFail: test.shouldFail})
+        s.onload = () => updateTable({name, testData})
+        s.onerror = (error) => updateTable({name, testData, error})
 
         document.body.appendChild(s)
-
     }
 }   
 
-function updateTable ({name, test, error='', notes = '', shouldFail}) {
-
+function updateTable ({name, testData, error}) {
 
     const table = document.getElementById('results-table')
     const row = table.insertRow(-1)
-
     const testName = row.insertCell(0)
     const loaded = row.insertCell(1)
     const passed = row.insertCell(2)
     const note = row.insertCell(3)
 
-    // set default values
+    // set default values and colors
     testName.innerText = name
     loaded.innerText = 'failed'
     passed.innerText = 'failed'
     row.style.backgroundColor = '#f97268'
     note.style.backgroundColor = "#ffff"
 
-    if (!error || shouldFail) {
+    if (!error || testData.shouldFail) {
         loaded.innerText = "pass"
 
-        const result = test()
+        const result = testData.test()
         if (result) {
             passed.innerText = "pass"
             row.style.backgroundColor = '#71bf69'
@@ -43,25 +40,32 @@ function updateTable ({name, test, error='', notes = '', shouldFail}) {
         }
     }
 
-    if (notes) {
-        note.innerText = notes
+    if (testData.notes) {
+        note.innerText = testData.notes
+    }
+
+    if (testData.cleanup) {
+        testData.cleanup()
     }
 }
 
 const surrogates = {
-    'google-analytics, analytics.js, crossOrigin': {
+    "google-analytics.com/analytics.js, crossOrigin" : {
         url: "https://google-analytics.com/analytics.js",
         crossOrigin: "anonymous",
         notes: "Test loading with crossOrigin set on element (should fail on Firefox) https://bugzilla.mozilla.org/show_bug.cgi?id=1694679",
-        test: () => { return window.ga.answer === 42 }
+        test: () => { return window.ga.answer === 42 },
+        cleanUp: () => { delete window.ga }
     },
-    'google-analytics, analytics.js': {
+    "google-analytics.com/analytics.js": {
         url: "https://google-analytics.com/analytics.js",
-        test: () => { return window.ga.answer === 42 }
+        test: () => { return window.ga.answer === 42 },
+        cleanUp: () => { delete window.ga }
     },
     'google-analytics, ga.js': {
         url: "https://google-analytics.com/ga.js",
-        test: () => { return !!window._gaq }
+        test: () => { return !!window._gaq },
+        cleanUp: () => { delete window._gaq }
     },
     'Directly accessing a web resouce' : {
         url: "chrome-extension://lfpgfgegioonagopmelghcelfgffmjnh/web_accessible_resources/analytics.js",

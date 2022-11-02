96cfe33ac8c94720c53bc09645453931a80808f4
Konrad Dzwinel <kdzwinel@gmail.com>
2022-11-02 23:48:54 +0100

    More from chormium.

--- features/client-hints/all-ch.js
+++ features/client-hints/all-ch.js
@@ -1,13 +1,24 @@
-// source: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers#client_hints
-const ALL_CH = ['Sec-CH-UA',
+// sources: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers#client_hints, https://chromium.googlesource.com/chromium/src/+/refs/heads/main/services/network/public/cpp/client_hints.cc, https://chromium.googlesource.com/chromium/src/+/refs/heads/main/components/client_hints/README.md
+const ALL_CH = [
+    'Sec-CH-UA',
     'Sec-CH-UA-Arch',
     'Sec-CH-UA-Bitness',
+    'Sec-CH-UA-Full',
     'Sec-CH-UA-Full-Version',
     'Sec-CH-UA-Full-Version-List',
     'Sec-CH-UA-Mobile',
     'Sec-CH-UA-Model',
     'Sec-CH-UA-Platform',
+    'Sec-CH-UA-Reduced',
+    'Sec-CH-UA-WoW64',
     'Sec-CH-UA-Platform-Version',
+    'Sec-CH-Prefers-Color-Scheme',
+    'Sec-CH-Prefers-Reduced-Motion',
+    'Sec-CH-Viewport-Height',
+    'Sec-CH-Device-Memory',
+    'Sec-CH-DPR',
+    'Sec-CH-Width',
+    'Sec-CH-Viewport-Width',
     'Content-DPR',
     'Device-Memory',
     'DPR',
@@ -18,7 +29,7 @@ const ALL_CH = ['Sec-CH-UA',
     'RTT',
     'Save-Data'].map(h => h.toLowerCase());
 
-// source: https://developer.mozilla.org/en-US/docs/Web/API/NavigatorUAData/getHighEntropyValues
+// sources: https://developer.mozilla.org/en-US/docs/Web/API/NavigatorUAData/getHighEntropyValues, https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/frame/navigator_ua_data.h
 const ALL_CH_JS = [
     // low-entropy
     'brands',
@@ -30,7 +41,8 @@ const ALL_CH_JS = [
     'model',
     'platformVersion',
     'uaFullVersion',
-    'fullVersionList'
+    'fullVersionList',
+    'wow64'
 ];
 
 if (typeof module === 'object' && typeof module.exports === 'object') {

1a3287d8d28741514802e641293a7b54ab66ab31
Maxim Tsoy <maks.tsoy@gmail.com>
2023-01-24 15:07:43 +0100

    Add a test page for cosmetic autoconsent rules

---
+++ features/autoconsent/banner.js
@@ -0,0 +1,20 @@
+const acceptButton = document.createElement('button');
+acceptButton.innerText = 'Accept all';
+acceptButton.id = 'accept-all';
+const banner = document.getElementById('privacy-test-page-cmp-test-banner');
+banner.appendChild(acceptButton);
+acceptButton.addEventListener('click', (ev) => {
+    ev.target.innerText = 'Accept was clicked!';
+    window.results.results.push('accept_button_clicked');
+});
+
+setTimeout(() => {
+    if (window.getComputedStyle(banner).display === 'none') {
+        window.results.results.push('banner_hidden');
+    }
+}, 500);
+
+window.results = {
+    page: 'autoconsent',
+    results: []
+};

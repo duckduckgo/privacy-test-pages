d2e5138759d72b7ad5446972fcf06d1cd71ada29
David Harbage <dave@duckduckgo.com>
2023-03-21 14:53:56 -0400

    Expand element hiding privacy test page (#130)
    * add csp
    
    * expand element hiding tests to cover improved heuristics

--- features/element-hiding/main.js
+++ features/element-hiding/main.js
@@ -7,10 +7,16 @@ const expectedResults = [
     { id: 'hide-empty-no-content', description: 'hide-empty rule: element containing no content is hidden', hidden: true },
     { id: 'hide-empty-ad-label', description: 'hide-empty rule: element containing ad label content is hidden', hidden: true },
     { id: 'hide-empty-delayed', description: 'hide-empty rule: empty element added to page after delay is hidden', hidden: true },
+    { id: 'hide-empty-pixel-element', description: 'hide-empty rule: element should be hidden when it contains tracking pixels', hidden: true },
     { id: 'closest-empty-single-div', description: 'closest-empty rule: element containing no content is hidden', hidden: true },
     { id: 'closest-empty-single-nested-div', description: 'closest-empty rule: parent of target element is hidden', hidden: true },
     { id: 'closest-empty-siblings-nested-div', description: 'closest-empty rule: parent of target element with empty siblings is hidden', hidden: true },
     { id: 'closest-empty-delayed-parent', description: 'closest-empty rule: parent of empty element added to page after delay is hidden', hidden: true },
+    { id: 'hide-empty-img-element', description: 'hide-empty rule: element should not be hidden when it contains image content', hidden: false },
+    { id: 'hide-empty-svg-element', description: 'hide-empty rule: element should not be hidden when it contains image content', hidden: false },
+    { id: 'hide-empty-input', description: 'hide-empty rule: input elements should not be hidden', hidden: false },
+    { id: 'hide-empty-form-element', description: 'hide-empty rule: elements containing forms should not be hidden', hidden: false },
+    { id: 'hide-empty-media-element', description: 'hide-empty rule: elements containing media should not be hidden', hidden: false },
     { id: 'hide-empty-text-content', description: 'hide-empty rule: element containing text content is not hidden', hidden: false },
     { id: 'hide-empty-frame-content', description: 'hide-empty rule: element containing iframe is not hidden', hidden: false },
     { id: 'hide-empty-delayed-frame', description: 'hide-empty rule: element that loads 3p frame after delay is not hidden', hidden: false },
@@ -18,7 +24,7 @@ const expectedResults = [
     { id: 'closest-empty-siblings-content-nested-div', description: 'closest-empty rule: parent of target element with non-empty siblings is not hidden', hidden: false },
     { id: 'closest-empty-frame-content', description: 'closest-empty rule: parent of element containing iframe is not hidden', hidden: false },
     { id: 'closest-empty-delayed-frame', description: 'closest-empty rule: parent of element that loads 3p frame after delay is not hidden', hidden: false },
-    { id: 'closest-empty-intermediate-element', description: 'closest-empty rule: intermediate elements should not be hidden when target is nested', hidden: false },
+    { id: 'closest-empty-img-element', description: 'closest-empty rule: element should not be hidden when it contains image content', hidden: false },
     { id: 'override-basic', description: 'override rule: element is not hidden when override rule present', hidden: false }
 ];
 
@@ -41,6 +47,9 @@ function init () {
     // inject frames and other content after delay
     injectDelayedContent();
 
+    // initialize canvas
+    initializeCanvas();
+
     // wait 2s then check results
     setTimeout(checkResults, 2000);
 }
@@ -48,7 +57,8 @@ function init () {
 function checkResults () {
     for (const testCase of expectedResults) {
         const element = document.getElementById(testCase.id);
-        const isHidden = element.style.getPropertyValue('display') === 'none';
+        const boundingRect = element.getBoundingClientRect();
+        const isHidden = boundingRect.height === 0 && boundingRect.width === 0;
         const result = testCase.hidden === isHidden;
         updateTable(testCase.id, testCase.description, result);
     }
@@ -78,6 +88,13 @@ function updateTable (name, description, testPassed) {
     }
 
     results.results.push(result);
+}
+
+function initializeCanvas () {
+    const canvas = document.getElementById('demo-canvas');
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = '#FF0000';
+    ctx.fillRect(0, 0, 80, 80);
 }
 
 function injectDelayedContent () {

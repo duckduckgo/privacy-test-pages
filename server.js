189e0bcca6206e0268827356452bfcbc034e30d0
Maxim Tsoy <maks.tsoy@gmail.com>
2023-12-20 12:00:02 +0100

    Add viewport test pages (#177)
    * Add viewport test pages
    
    * Clean up viewport cases
    
    * Add breadcrumbs to the viewport case template

--- server.js
+++ server.js
@@ -281,5 +281,8 @@ app.use('/features/client-hints', chRoutes);
 const clearDataRoutes = require('./features/clear-data/server/routes.js');
 app.use('/features/clear-data', clearDataRoutes);
 
+const viewportRoutes = require('./viewport/server/routes.js');
+app.use('/viewport', viewportRoutes);
+
 const addressBarSpoofingRoutes = require('./security/address-bar-spoofing/server/routes.js');
 app.use('/security/address-bar-spoofing', addressBarSpoofingRoutes);

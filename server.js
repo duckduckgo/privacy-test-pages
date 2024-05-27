162100a1c7cdee89ba4424ee2e808c7d60214e60
Anastasia Shuba <ashuba22@gmail.com>
2024-05-27 21:11:46 +0200

    lint

--- server.js
+++ server.js
@@ -265,19 +265,19 @@ app.get('/security/address-bar-spoofing/download-redirect', (req, res) => {
 app.get('/error', (req, res) => {
     const code = parseInt(req.query.code);
 
-    if (code == 401) {
+    if (code === 401) {
         // This sets a WebView error on Windows
         res.set('WWW-Authenticate', 'Basic realm="Restricted Area"');
     }
 
     if (isNaN(code) || code < 100 || code > 599) {
         res.statusCode = 200;
-        res.send(`Invalid HTTP error code provided. Status not set.`);
+        res.send('Invalid HTTP error code provided. Status not set.');
     } else {
         res.statusCode = code;
         res.send(`HTTP error code set to '${code}'`);
     }
-    res.end();    
+    res.end();
 });
 
 // Simulate connection drop

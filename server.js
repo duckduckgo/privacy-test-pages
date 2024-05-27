18c9b0f54ff9dd1bcc9ae091de1b5f5351a58b25
Anastasia Shuba <ashuba22@gmail.com>
2024-05-27 13:23:55 +0200

    Add pages to simulate HTTP/WebView errors

--- server.js
+++ server.js
@@ -261,6 +261,30 @@ app.get('/security/address-bar-spoofing/download-redirect', (req, res) => {
     res.redirect(301, 'https://staticcdn.duckduckgo.com/macos-desktop-browser/duckduckgo.dmg');
 });
 
+// Simulate HTTP errors
+app.get('/error', (req, res) => {
+    const code = parseInt(req.query.code);
+
+    if (code == 401) {
+        // This sets a WebView error on Windows
+        res.set('WWW-Authenticate', 'Basic realm="Restricted Area"');
+    }
+
+    if (isNaN(code) || code < 100 || code > 599) {
+        res.statusCode = 200;
+        res.send(`Invalid HTTP error code provided. Status not set.`);
+    } else {
+        res.statusCode = code;
+        res.send(`HTTP error code set to '${code}'`);
+    }
+    res.end();    
+});
+
+// Simulate connection drop
+app.get('/error/drop', (_, res) => {
+    res.destroy();
+});
+
 app.use('/content-scope-scripts/', express.static('node_modules/@duckduckgo/content-scope-scripts/integration-test/test-pages/'));
 
 const blockingRoutes = require('./privacy-protections/request-blocking/server/routes');

965608d80fd35f91e001f9c7be2a7fa35397b69b
Thomas Espach <tespach@duckduckgo.com>
2024-07-25 16:13:18 +0100

    Fix Lints

--- security/badware/server/routes.js
+++ security/badware/server/routes.js
@@ -1,59 +1,59 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 // Returns a 301 redirect to the main phishing test page
-router.get("/", (req, res) => {
-  res.redirect(
-    301,
-    "/security/badware/phishing.html"
-  );
+router.get('/', (req, res) => {
+    res.redirect(
+        301,
+        '/security/badware/phishing.html'
+    );
 });
 
 // Returns a 302 redirect to the main phishing test page
-router.get("/302", (req, res) => {
-  res.redirect(
-    302,
-    "/security/badware/phishing.html"
-  );
+router.get('/302', (req, res) => {
+    res.redirect(
+        302,
+        '/security/badware/phishing.html'
+    );
 });
 
 // Returns a 301 redirect to a JS redirector page
-router.get("/js", (req, res) => {
+router.get('/js', (req, res) => {
     res.redirect(
         301,
-        "/security/badware/phishing-js-redirector.html"
-    )
+        '/security/badware/phishing-js-redirector.html'
+    );
 });
 
 // Returns a 301 redirect to a JS redirector helper page
-router.get("/js2", (req, res) => {
+router.get('/js2', (req, res) => {
     res.redirect(
         301,
-        "/security/badware/phishing-js-redirector-helper.html"
-    )
+        '/security/badware/phishing-js-redirector-helper.html'
+    );
 });
 
 // Returns a redirect to a page that loads an iframe that renders a phishing page
-router.get("/iframe", (req, res) => {
+router.get('/iframe', (req, res) => {
     res.redirect(
         301,
-        "/security/badware/phishing-iframe-loader.html"
-    )
+        '/security/badware/phishing-iframe-loader.html'
+    );
 });
 
 // Returns a redirect to a page that loads multiple iframes to attempt to bypass the phishing detection
-router.get("/iframe2", (req, res) => {
-  res.redirect(301, "/security/badware/phishing-legit-iframe-loader.html");
+router.get('/iframe2', (req, res) => {
+    res.redirect(301, '/security/badware/phishing-legit-iframe-loader.html');
 });
 
 // Returns a redirect to a page that renders a phishing page using a meta refresh (not flagged in dataset)
-router.get("/meta", (req, res) => {
-  res.redirect(301, "/security/badware/phishing-meta-redirect-clean.html");
+router.get('/meta', (req, res) => {
+    res.redirect(301, '/security/badware/phishing-meta-redirect-clean.html');
 });
 
 // Returns a redirect to a page that renders a phishing page using a meta refresh (flagged in dataset)
-router.get("/meta2", (req, res) => {
-  res.redirect(301, "/security/badware/phishing-meta-redirect.html");
+router.get('/meta2', (req, res) => {
+    res.redirect(301, '/security/badware/phishing-meta-redirect.html');
 });
 
 module.exports = router;

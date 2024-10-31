e5a86d2513e2840b40e3975ecf32ef87e04ff9f0
Jonathan Kingston <jkingston@duckduckgo.com>
2024-10-31 12:32:23 +0000

    Add support for suggested filename

--- features/download/server/routes.js
+++ features/download/server/routes.js
@@ -5,21 +5,22 @@ function downloadPDF (res) {
     res.download('./features/download/download.pdf');
 }
 
-function downloadJSON (res) {
+function downloadJSON (res, suggestedFilename) {
     const json = JSON.stringify({ example: 'test' });
     const buf = Buffer.from(json);
     res.writeHead(200, {
         'Content-Type': 'application/octet-stream',
-        'Content-disposition': 'attachment; filename=data.json'
+        'Content-disposition': 'attachment; filename=' + suggestedFilename
     });
     res.write(buf);
     res.end();
 }
 
 router.get('/file/:type', (req, res) => {
+    const suggestedFilename = req.query.suggestedFilename || 'data.json'
     switch (req.params.type) {
     case 'json':
-        downloadJSON(res);
+        downloadJSON(res, suggestedFilename);
         break;
     case 'pdf':
         downloadPDF(res);

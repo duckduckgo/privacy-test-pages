d652d9bc95de7e351c7ad12db45c04c32c6c51a4
Thom Espach <tespach@duckduckgo.com>
2024-11-20 11:45:15 +0000

    Add address bar spoofing test (#243)
    * Add address bar spoofing test

--- security/address-bar-spoofing/server/routes.js
+++ security/address-bar-spoofing/server/routes.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 // Returns a 301 redirect to a download link of our browser
 // for use in the download path test
-router.get('/', (req, res) => {
+router.get('/download-redirect', (req, res) => {
     res.redirect(301, 'https://staticcdn.duckduckgo.com/macos-desktop-browser/duckduckgo.dmg');
+});
+
+// Returns a 204 no content
+router.get('/no-content', (req, res) => {
+    res.status(204).send();
 });
 
 module.exports = router;

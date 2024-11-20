9eea3c343ef40db0182c44fb3711d22c4418364c
Thomas Espach <tespach@duckduckgo.com>
2024-11-20 11:43:55 +0000

    lint-js

--- security/address-bar-spoofing/server/routes.js
+++ security/address-bar-spoofing/server/routes.js
@@ -7,7 +7,7 @@ router.get('/download-redirect', (req, res) => {
     res.redirect(301, 'https://staticcdn.duckduckgo.com/macos-desktop-browser/duckduckgo.dmg');
 });
 
-// Returns a 204 no content 
+// Returns a 204 no content
 router.get('/no-content', (req, res) => {
     res.status(204).send();
 });

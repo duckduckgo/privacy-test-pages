6a93536d07d206edcb929ed6c2014264780ce46f
Maxim Tsoy <maks.tsoy@gmail.com>
2024-12-19 13:45:47 +0100

    Add a test page for CPM filterlist (#248)
    * Add a test page for CPM filterlist
    
    * Add a dedicated index page for CPM

---
+++ features/autoconsent/filterlist.js
@@ -0,0 +1,20 @@
+const acceptButton = document.createElement('button');
+acceptButton.innerText = 'Accept all';
+acceptButton.id = 'accept-all';
+const banner = document.quesrySelector('#privacy-test-page-cmp-test-banner-filterlist');
+banner.appendChild(acceptButton);
+acceptButton.addEventListener('click', (ev) => {
+    ev.target.innerText = 'Accept was clicked!';
+    window.results.results.push('accept_button_clicked');
+});
+
+setTimeout(() => {
+    if (window.getComputedStyle(banner).opacity === '0') {
+        window.results.results.push('banner_hidden');
+    }
+}, 500);
+
+window.results = {
+    page: 'autoconsent-filterlist',
+    results: []
+};

49aaa1d74798512debcca4850c0dd700b2663164
Maxim Tsoy <maks.tsoy@gmail.com>
2025-12-02 15:26:34 +0100

    add coments

--- features/autoconsent/reload-loop.js
+++ features/autoconsent/reload-loop.js
@@ -6,6 +6,7 @@ document.getElementById('privacy-test-page-cmp-test').appendChild(button);
 button.addEventListener('click', (ev) => {
     ev.target.innerText = 'I was clicked!';
     window.results.results.push('button_clicked');
+    // reload the page to simulate a reload loop
     location.reload();
 });
 
@@ -16,6 +17,7 @@ document.getElementById('privacy-test-page-cmp-test').appendChild(acceptButton);
 acceptButton.addEventListener('click', (ev) => {
     ev.target.innerText = 'Accept was clicked!';
     window.results.results.push('accept_button_clicked');
+    // reload the page to simulate a reload loop
     location.reload();
 });
 

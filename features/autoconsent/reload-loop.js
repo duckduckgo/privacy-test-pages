d2b5aeb2ceda0c5e6f64a1de9721f9b242f72b50
Maxim Tsoy <maks.tsoy@gmail.com>
2025-12-02 15:23:15 +0100

    Add a test page for CPM reload loop detection

---
+++ features/autoconsent/reload-loop.js
@@ -0,0 +1,25 @@
+// dummy CMP
+const button = document.createElement('button');
+button.innerText = 'Reject all';
+button.id = 'reject-all';
+document.getElementById('privacy-test-page-cmp-test').appendChild(button);
+button.addEventListener('click', (ev) => {
+    ev.target.innerText = 'I was clicked!';
+    window.results.results.push('button_clicked');
+    location.reload();
+});
+
+const acceptButton = document.createElement('button');
+acceptButton.innerText = 'Accept all';
+acceptButton.id = 'accept-all';
+document.getElementById('privacy-test-page-cmp-test').appendChild(acceptButton);
+acceptButton.addEventListener('click', (ev) => {
+    ev.target.innerText = 'Accept was clicked!';
+    window.results.results.push('accept_button_clicked');
+    location.reload();
+});
+
+window.results = {
+    page: 'autoconsent',
+    results: []
+};
